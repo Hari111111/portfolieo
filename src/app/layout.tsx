c7d266a932aa21c900109d04a7d671b8656319c4
Hari Mishra <hari@websultanate.com>
2025-12-09 17:38:13 +0530

    chnages verson

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,39 +6,34 @@ import { ThemeProvider } from "next-themes";
 import ScrollToTop from '@/components/ScrollToTop';
 import Aoscompo from "@/utils/aos";
 import NextTopLoader from 'nextjs-toploader';
-import SessionProviderComp from "@/components/nextauth/SessionProvider";
 import { AuthDialogProvider } from "./context/AuthDialogContext";
 const inter = Inter({ subsets: ["latin"] });
 
 
 export default function RootLayout({
   children,
-  session,
 }: Readonly<{
   children: React.ReactNode;
-  session: any
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <NextTopLoader />
         <AuthDialogProvider>
-          <SessionProviderComp session={session}>
-            <ThemeProvider
-              attribute="class"
-              enableSystem={true}
-              defaultTheme="system"
-            >
-              <Aoscompo>
-                <Header />
-                <div className="pt-20">
-                  {children}
-                </div>
-                <Footer />
-              </Aoscompo>
-              <ScrollToTop />
-            </ThemeProvider>
-          </SessionProviderComp>
+          <ThemeProvider
+            attribute="class"
+            enableSystem={true}
+            defaultTheme="system"
+          >
+            <Aoscompo>
+              <Header />
+              <div className="pt-20">
+                {children}
+              </div>
+              <Footer />
+            </Aoscompo>
+            <ScrollToTop />
+          </ThemeProvider>
         </AuthDialogProvider>
       </body>
     </html>
