e9a6fe2bd7e833a1fd692c9caa1be99a0c732e4d
Hari Mishra <hari@websultanate.com>
2026-03-13 14:22:17 +0530

    chnage base url

--- src/utils/axiosHelper.ts
+++ src/utils/axiosHelper.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosHelper = axios.create({
-    baseURL: process.env.API_BASE_URL || "http://localhost:5000/api",
+    baseURL: "https://port-backend-three.vercel.app/api",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
