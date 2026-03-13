d1dfd4ce099a008cce082722af4c8888eed13f2c
Hari Mishra <hari@websultanate.com>
2026-03-13 14:35:31 +0530

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import ScrollToTop from '@/components/ScrollToTop';
 import Aoscompo from "@/utils/aos";
 import NextTopLoader from 'nextjs-toploader';
 import { AuthDialogProvider } from "./context/AuthDialogContext";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 
@@ -18,6 +19,19 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+        </Script>
         <NextTopLoader />
         <AuthDialogProvider>
           <ThemeProvider
